dea38d7b45d45967d5d9b953c2278a22e43cba29
dylanmlilo <dylanmlilo12@gmail.com>
2024-06-13 10:47:53 +0000

    0x00-MySQL_Advanced

---
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -0,0 +1,3 @@
+-- Assuming the table names is already imported and exists
+-- Create an index on the first letter of name and the score column
+CREATE INDEX idx_name_first_score ON names (name(1), score)
---
+++ 10-div.sql
@@ -0,0 +1,17 @@
+-- Create a function SafeDiv
+DELIMITER $$ ;
+CREATE FUNCTION SafeDiv(
+	a INT,
+	b INT
+)
+RETURNS FLOAT
+DETERMINISTIC
+BEGIN
+	DECLARE result FLOAT;
+	IF b = 0 THEN
+		RETURN 0;
+        END IF;
+        SET result = (a * 1.0) / b;
+        RETURN result;
+END;$$
+DELIMITER ;
